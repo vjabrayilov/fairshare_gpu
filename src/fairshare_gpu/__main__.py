ee4e912f1abbcbc2e3fb9181916b4bb6bd0a1a2f
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-12-14 19:08:49 -0500

    add copyright to files Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- src/fairshare_gpu/__main__.py
+++ src/fairshare_gpu/__main__.py
@@ -1,4 +1,9 @@
+# Copyright (c) 2025 Vahab Jabrayilov (vjabrayilov@cs.columbia.edu)
+# Copyright (c) 2025 DAPLab of Columbia University (https://daplab.cs.columbia.edu/)
+# Copyright (c) 2025 The Trustees of Columbia University in the City of New York
+
 from .cli import main
+
 
 if __name__ == '__main__':
     main()
--- src/fairshare_gpu/config.py
+++ src/fairshare_gpu/config.py
@@ -1,4 +1,9 @@
+# Copyright (c) 2025 Vahab Jabrayilov (vjabrayilov@cs.columbia.edu)
+# Copyright (c) 2025 DAPLab of Columbia University (https://daplab.cs.columbia.edu/)
+# Copyright (c) 2025 The Trustees of Columbia University in the City of New York
+
 from __future__ import annotations
+
 
 from pathlib import Path
 from typing import Dict, List, Literal, Optional
